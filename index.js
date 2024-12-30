a17e2bee98e1677ab15ed7b0ef0f89395f912c2c
if164ever <zadorozhniak.ihor@gmail.com>
2024-12-29 20:33:47 -0600

    generate a keys

---
+++ index.js
@@ -0,0 +1,17 @@
+const {
+    Connection,
+    PublicKey,
+    clusterApiUrl,
+    Keypair,
+    LAMPORTS_PER_SOL
+} = require("@solana/web3.js")
+
+
+const wallet = new Keypair();
+
+const publicKey = new PublicKey(wallet._keypair.publicKey);
+
+const secretKey = wallet._keypair.secretKey;
+
+console.log(publicKey);
+console.log(secretKey);
